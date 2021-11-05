921e029149c019b74270d5f26100384ff22f312c
karimmango <karim.mango47@gmail.com>
2021-11-05 19:39:48 +0100

    heroku

--- coolpress/press/gravatar.py
+++ coolpress/press/gravatar.py
@@ -5,5 +5,7 @@ from libgravatar import Gravatar
 
 
 def get_gravatar(email: str) -> Optional[str]:
+    if not email:
+        return None
     grav = Gravatar(email)
     return grav.get_image(size=200, use_ssl=True, default='', force_default=False, filetype_extension=False,  rating='', )
--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -1,9 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.post_list, name='index'),
+    url(r'^$', views.post_list, name='index'),
     path('post/<int:post_id>', views.post_detail, name='posts-detail'),
     path('posts/', views.post_list, name='posts-list'),
     path('post/add/', views.post_update, name='post-add'),
